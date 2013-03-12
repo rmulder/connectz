9df4925345cc58226ca52c740d2741b6523cb096
Robert Mulder <rmulder@xmission.com>
2013-03-12 10:27:05 -0700

    Meetz code updates

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,12 @@
  * under the License.
  */
 var app = {
+    watchID: null,
+    map: null,
+    myLocationMarker: null,
+    searchCircle: null,
+    myLocation: null,
+
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -34,6 +40,7 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        app.startGPS();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -54,14 +61,27 @@ var app = {
                     console.log(contacts);
                     //alert('contacts.length:' + contacts.length);
                     var tmp = '', fi, li;
-                    for (var i=0; i<contacts.length; i++) {
+                    for (var i = 0; i < contacts.length; i++) {
                         tmp += '<tr>';
                         fi = (contacts[i].name.givenName) ? contacts[i].name.givenName.substring(0,1) : '';
                         li = (contacts[i].name.familyName) ? contacts[i].name.familyName.substring(0,1) : '';
+                        tmp += '<td><input type="checkbox" name="meetz" value=""/></td>';
                         tmp += '<td>' + fi + li + '</td>';
                         tmp += '<td>' + contacts[i].name.givenName + ' ' + contacts[i].name.familyName + '</td>';
+                        if (contacts[i].phoneNumbers) {
+                          for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+                            var phone = contacts[i].phoneNumbers[j];
+                            console.log('phoneNumber:');
+                            console.log(phone);
+                            if (phone.type === 'mobile' || phone.type === 'other') {
+                              tmp += '<td><a href="tel:' + phone.value + '">Call</a> <a href="sms:' + phone.value + '?body=Meetz: "> Text' + '</a></td>';
+                            }
+                          }
+                        }
                         tmp += '</tr>';
+                        console.log('emails:');
                         console.log(contacts[i].emails);
+                        console.log('phone numbers:');
                         console.log(contacts[i].phoneNumbers);
                     }
                     $('#contacts-body').html(tmp);
@@ -72,5 +92,66 @@ var app = {
                 options);
         
         console.log('Received Event: ' + id);
+    },
+
+    startGPS: function() {
+        console.log('begin: startGPS');
+        // simulate getting position from GPS
+        app.myLocation = new google.maps.LatLng(39, -90);
+        console.log(app.myLocation);
+        
+        // create Google map
+        var mapOptions = {
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: app.myLocation
+        };
+        console.log(mapOptions);
+        app.map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        console.log(app.map);
+        google.maps.event.trigger(app.map, 'resize');
+        
+        app.myLocationMarker = new google.maps.Marker({
+                                                  title: 'This is me!',
+                                                  zIndex: 90,
+                                                  optimized: false,
+                                                  map: app.map,
+                                                  position: app.myLocation
+                                                  });
+        
+        app.searchCircle = new google.maps.Circle({
+                                              fillColor: '#c0e4dd',
+                                              strokeColor: '#f15f22',
+                                              fillOpacity: 0.5,
+                                              radius: 1500,
+                                              map: app.map,
+                                              center: app.myLocation
+                                              });
+        // using bindTo instead does not fix the problem
+        //  searchCircle.bindTo('center', myLocationMarker, 'position'); //This will set the circle bound to the marker at center
+        console.log('end: startGPS');
+    },
+    
+    MoveMarkerAndCircle: function(lat, lng) {
+        console.log('begin: MoveMarkerAndCircle');
+        app.myLocation = new google.maps.LatLng(lat, lng);
+        app.myLocationMarker.setPosition(app.myLocation);
+        
+        // set circle invisible before moving and visible after does fix redraw for 4.1.1
+        // but causes some annoying flashing of the circle for 4.1.1 and everything else
+        //  searchCircle.setVisible(false);
+        app.searchCircle.setCenter(app.myLocation);
+        //  searchCircle.setVisible(true);
+        console.log('end: MoveMarkerAndCircle');
+    },
+    
+    GenerateFakeMovement: function() {
+        console.log('begin: GenerateFakeMovement');
+        var currentPosition = app.myLocationMarker.getPosition();
+        var newLat = currentPosition.lat() + 0.001;
+        var newLng = currentPosition.lng() + 0.001;
+        app.MoveMarkerAndCircle(newLat, newLng);
+        console.log('end: GenerateFakeMovement');
     }
+    
 };
